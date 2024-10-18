3abb84fd886cdcc014b6768537c2eccdf83a6ec0
DamithTennakoon <88393680+DamithTennakoon@users.noreply.github.com>
2024-10-18 13:55:11 -0400

    Collecting joint-joint distance data for gripper control gesture

--- Assets/C# Scripts/Data Collection/collect_joint_distances.cs
+++ Assets/C# Scripts/Data Collection/collect_joint_distances.cs
@@ -1,7 +1,7 @@
 // Objective: Record the distances between specific joints.
 // Dependencies: <joint_storage.cs>
 // Author: Damith Tennakoon
-// NOTE: AS A PART OF HRI VERSION II (SECOND YEAR OF MASTER'S)
+// NOTE: AS A PART OF HRI VERSION II (SECOND YEAR OF MASTERS)
 
 using System.Collections;
 using System.Collections.Generic;
@@ -25,21 +25,20 @@ public class collect_joint_distances : MonoBehaviour
 
     // Store distance values
     [SerializeField] private float dist4_8_rh = 0.0f;
+    [SerializeField] private float dist8_0_rh = 0.0f;
     [SerializeField] private float dist12_0_rh = 0.0f;
     [SerializeField] private float dist16_0_rh = 0.0f;
     [SerializeField] private float dist20_0_rh = 0.0f;
 
     // Define the name of the .txt file
-    private string fileName = "TESTING_TESTING.txt";
+    private string fileName = "open_gripper_gesture.txt";
 
     void Start()
     {
         // Initialize the header of the .CSV file
         using (StreamWriter writer = new StreamWriter(fileName, true))
         {
-            writer.WriteLine("Time," +
-                "Dist4_8," +
-                "Dist12_0,Dist16_0,Dist20_0");
+            writer.WriteLine("Time,Dist8_0,Dist12_0,Dist16_0,Dist20_0");
         }
     }
 
@@ -48,13 +47,14 @@ public class collect_joint_distances : MonoBehaviour
     {
         // Compute the distances to joint combinations
         dist4_8_rh = Vector3.Distance(jointStorage.rightHandJoints[4].position, jointStorage.rightHandJoints[8].position);
-
+        dist8_0_rh = Vector3.Distance(jointStorage.rightHandJoints[8].position, jointStorage.rightHandJoints[0].position);
         dist12_0_rh = Vector3.Distance(jointStorage.rightHandJoints[12].position, jointStorage.rightHandJoints[0].position);
         dist16_0_rh = Vector3.Distance(jointStorage.rightHandJoints[16].position, jointStorage.rightHandJoints[0].position);
         dist20_0_rh = Vector3.Distance(jointStorage.rightHandJoints[20].position, jointStorage.rightHandJoints[0].position);
 
+
         // Record data
-        if (dist4_8_rh > 0.0)
+        if (dist12_0_rh > 0.0)
         {
             addRecord(fileName);
         }
@@ -71,9 +71,7 @@ public class collect_joint_distances : MonoBehaviour
             {
                 // Write values to file
                 file.Write(elapsedTime.ToString() + ",");
-
-                file.Write(dist4_8_rh.ToString() + ",");
-
+                file.Write(dist8_0_rh.ToString() + ",");
                 file.Write(dist12_0_rh.ToString() + ",");
                 file.Write(dist16_0_rh.ToString() + ",");
                 file.WriteLine(dist20_0_rh.ToString());
